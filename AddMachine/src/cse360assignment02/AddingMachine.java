8387dc9b1a8a5de4f9529e80a0eec3d35c82962c
malin <malin@192.168.1.28>
2020-10-05 13:41:30 -0700

    Adding comments and documentation of the methods

---
+++ AddMachine/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,68 @@
+package cse360assignment02;
+
+/**
+ * The AddingMachine program implements an application that either adds,
+ * subtracts, and displays the number that is given to the program.
+ *
+ * @author Malin Tan
+ * @version 1.0
+ * @since 2020-10-05 
+ * Email: mtan12@asu.edu 
+ * ID: 1216503951
+ */
+public class AddingMachine {
+	private int total;
+
+	public AddingMachine() {
+		total = 0; // not needed - included for clarity
+	}
+
+	/**
+	 * This method is a helper method that returns the current total.
+	 * 
+	 * @return int This returns total variable.
+	 */
+	public int getTotal() {
+		return 0;
+	}
+
+	/**
+	 * This method is used to add two integers. This method adds an integer to the
+	 * total variable.
+	 * 
+	 * @param value This is the first paramter to add method
+	 * @return Nothing.
+	 */
+	public void add(int value) {
+	}
+
+	/**
+	 * This method is used to subtracts two integers. This method subtracts an
+	 * integer to the total variable.
+	 * 
+	 * @param value This is the first paramter to subtract method
+	 * @return Nothing.
+	 */
+	public void subtract(int value) {
+	}
+
+	/**
+	 * This method is a toString method that returns a string that documents that
+	 * history of transactions, and separates all operations and values with a
+	 * space.
+	 * 
+	 * @return String This returns a formatted string.
+	 */
+	public String toString() {
+		return "";
+	}
+
+	/**
+	 * This method is used to subtracts two integers. This method clears the memory
+	 * of all the transactions.
+	 * 
+	 * @return Nothing.
+	 */
+	public void clear() {
+	}
+}
